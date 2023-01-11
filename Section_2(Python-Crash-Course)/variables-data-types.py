f4b229070c3adc6781310ef652692412b9b7a1f9
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-11 16:17:52 +0000

    Tenth Commit: Add Tutorial -> Assigning values to Variables in Python

--- Section_2(Python-Crash-Course)/variables-data-types.py
+++ Section_2(Python-Crash-Course)/variables-data-types.py
@@ -1,1 +1,26 @@
 # Variables and Data Types
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Variables, Constants and Literals
+# URL: https://www.programiz.com/python-programming/variables-constants-literals
+
+# Python Variables, Constants, Literals and print()
+# Definition: Variables are used to store values
+
+# (1) Assigning values to Variables in Python
+# Assign (=) value "10" to numbers variable
+numbers = 10
+# Print Output
+print(10)
+
+
+# (2) Changing the Value of a Variable in Python
+# (3) Assigning multiple values to multiple variables
+# (4) Rules for Naming Python Variables
+# (5) Python Constants
+# (6) Python Literals
+# (7) Python Numeric Literals
+# (8) Python Boolean Literals
+# (9) String and Character Literals in Python
+# (10) Special Literal in Python
+# (11) Literal Collections
