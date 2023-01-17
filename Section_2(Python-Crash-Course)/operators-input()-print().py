18f9f9381ed53ea983496cacbb8e57a7424ca015
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-17 11:50:43 +0000

    Twenty-Seven Commit: Create skeleton operators-input()-print().py file

---
+++ Section_2(Python-Crash-Course)/operators-input()-print().py
@@ -0,0 +1,5 @@
+# Python Operators
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Variables, Constants and Literals
+# URL: https://www.programiz.com/python-programming/variables-constants-literals
