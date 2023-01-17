82f9787d263f960f796d3365bb85ddc8a5fb38ee
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-17 12:07:28 +0000

    Twenty-Eight Commit: Add Tutorial -> Python input() function

--- Section_2(Python-Crash-Course)/operators-input()-print().py
+++ Section_2(Python-Crash-Course)/operators-input()-print().py
@@ -1,5 +1,32 @@
-# Python Operators
+# Python Operators, input() and print()
 # ========================
 # Adapted From: Programiz
 # Tutorial Title: Python Variables, Constants and Literals
 # URL: https://www.programiz.com/python-programming/variables-constants-literals
+
+# Python Basic Input and Output
+# 1. Python print()
+
+# 2. Python input()
+# Definition: In Python, we use the input() function to take input from the user.
+# Syntax: input(prompt)
+# using input() to take user input
+num = input('What is your date of birth? : ')
+print('You Entered:', num)
+print('Data type of num:', type(num))
+# In the above example, we have used the input() function to take input from the user and stored the user input in the num variable.
+# It is important to note that the entered value at the prompt is a string, not a number. So, type(num) returns <class 'str'>.
+# To convert user input into a number we can use int() or float() functions as:
+num = int(input('What is your date of birth?'))
+print('You Entered:', num)
+print('Data type of num:', type(num))
+
+# Python Operators
+# Operators are special symbols that perform operations on variables and values.
+# Types of Python Operators
+# 1. Arithmetic Operators
+# 2. Assignment Operators
+# 3. Comparison Operators
+# 4. Logical Operators
+# 5. Bitwise Operators
+# 6. Special Operators
