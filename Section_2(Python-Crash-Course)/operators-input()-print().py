9a021c0cb61465bbc4829f891401be38d20f8524
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-17 12:24:41 +0000

    Thirty Commit: Add Tutorial -> Python Arithmetic Operators

--- Section_2(Python-Crash-Course)/operators-input()-print().py
+++ Section_2(Python-Crash-Course)/operators-input()-print().py
@@ -51,6 +51,31 @@ print('Data type of num:', type(num))
 # Operators are special symbols that perform operations on variables and values.
 # Types of Python Operators
 # 1. Arithmetic Operators
+# Arithmetic operators are used to perform mathematical operations like addition, subtraction, multiplication, etc. For example,
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)
+
+# subtraction
+print ('Subtraction: ', a - b)
+
+# multiplication
+print ('Multiplication: ', a * b)
+
+# division
+print ('Division: ', a / b)
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)
+
+# a to the power b
+print ('Power: ', a ** b)
+
 # 2. Assignment Operators
 # 3. Comparison Operators
 # 4. Logical Operators
