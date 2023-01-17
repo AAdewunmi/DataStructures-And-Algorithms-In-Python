9f54d0c2ec8fee7ad8a638b3b8aba843f2d7af84
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-17 13:03:50 +0000

    Thirty-Three Commit: Add Tutorial -> Python Bitwise Operators

--- Section_2(Python-Crash-Course)/operators-input()-print().py
+++ Section_2(Python-Crash-Course)/operators-input()-print().py
@@ -137,4 +137,18 @@ print((a > 2) and (b >= 6))    # True
 # True if the operand is False and vice-versa.
 
 # 5. Bitwise Operators
+# Definition: Bitwise operators act on operands as if they were strings of binary digits. They operate bit by bit, hence the name.
+# For example, 2 is 10 in binary and 7 is 111.
+
+# In the table below: Let x = 10 (0000 1010 in binary) and y = 4 (0000 0100 in binary)
+
+# Operator:     Meaning:      Example:
+
+# &           Bitwise AND      x & y = 0 (0000 0000)
+# |            Bitwise OR      x | y = 14 (0000 1110)
+# ~           Bitwise NOT      ~x = -11 (1111 0101)
+# ^           Bitwise XOR      x ^ y = 14 (0000 1110)
+# >>      Bitwise right shift  x >> 2 = 2 (0000 0010)
+# <<      Bitwise left shift   x << 2 = 40 (0010 1000)
+
 # 6. Special Operators
