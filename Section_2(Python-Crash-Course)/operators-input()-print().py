da4dfa1d7e3879cd515135e05a5003f9800805fd
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-17 13:58:11 +0000

    Thirty-Five Commit: Add Tutorial -> Python Membership operators

--- Section_2(Python-Crash-Course)/operators-input()-print().py
+++ Section_2(Python-Crash-Course)/operators-input()-print().py
@@ -179,3 +179,30 @@ print(x3 is y3)  # prints False
 # But x3 and y3 are lists. They are equal but not identical. It is because the interpreter locates them separately in memory although they are equal.
 
 # 2. Membership operators
+# Definition: In Python, in and not in are the membership operators. They are used to test whether a value or variable is found in a sequence (string, list, tuple, set and dictionary).
+
+# In a dictionary we can only test for presence of key, not the value.
+
+#  Operator:          Meaning:                                  Example:
+# in True    if value/variable is found in the sequence          5 in x
+# not in   True if value/variable is not found in the sequence  5 not in x
+
+# Example: Membership operators in Python
+
+x = 'Hello world'
+y = {1:'a', 2:'b'}
+
+# check if 'H' is present in x string
+print('H' in x)  # prints True
+
+# check if 'hello' is present in x string
+print('hello' not in x)  # prints True
+
+# check if '1' key is present in y
+print(1 in y)  # prints True
+
+# check if 'a' key is present in y
+print('a' in y)  # prints False
+
+# Here, 'H' is in x but 'hello' is not present in x (remember, Python is case sensitive).
+ # Similarly, 1 is key and 'a' is the value in dictionary y. Hence, 'a' in y returns False.
