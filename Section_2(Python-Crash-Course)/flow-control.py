d1a368ffaf4771abc0e06fa69bd6c0f87afd1213
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-18 15:42:16 +0000

    Thirty-Eight Commit: Add Tutorial -> Python 'if ... else' statement

--- Section_2(Python-Crash-Course)/flow-control.py
+++ Section_2(Python-Crash-Course)/flow-control.py
@@ -5,10 +5,10 @@
 # URL: https://www.programiz.com/python-programming
 
 # 1. Python if ... else
+# ----------------------
 # In Python, there are three forms of the 'if ... else' statement:
 # i. if statement
 # Example 1: Python if Statement
-
 number = 10
 # check if number is greater than 0
 if number > 0:
@@ -16,8 +16,24 @@ if number > 0:
 print('The if statement is easy')
 
 # ii. if ... else statement
+# Example 2. Python if...else Statement
+number = 10
+# check if number is greater than 0
+if number > 0:
+    print('Positive number')
+else:
+    print('Negative number')
+print('This statement is always executed')
+
 # iii.if ... elif ... else statement
+
+
+
 # 2. Python for Loop
+# -------------------
 # 3. Python while Loop
+# ----------------------
 # 4. Python break and continue
+# -----------------------------
 # 5. Python Pass
+# ---------------
