15e5fa9cf7609f891fa1d8d6155c4a2743a9a063
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-19 16:54:12 +0000

    Fifty-Three Commit: Add Tutorial -> Python Pass

--- Section_2(Python-Crash-Course)/flow-control.py
+++ Section_2(Python-Crash-Course)/flow-control.py
@@ -166,3 +166,12 @@ while num < 10:
 
 # 6. Python Pass
 # ---------------
+# Definition: In Python programming, the pass statement is a null statement which can be used as a placeholder for future code.
+# Suppose we have a loop or a function that is not implemented yet, but we want to implement it in the future. In such cases, we can use the pass statement.
+# Syntax: pass
+# Example 1: Using pass With Conditional Statement
+n = 10
+# use pass inside if statement
+if n > 10:
+    pass
+print('Hello')
