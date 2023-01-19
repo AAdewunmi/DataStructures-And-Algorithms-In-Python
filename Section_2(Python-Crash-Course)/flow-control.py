f6028babae336b39661f2c37d108d1f172ac4e93
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-19 16:41:20 +0000

    Fifty Commit: Add Tutorial -> Python break Statement with while Loop

--- Section_2(Python-Crash-Course)/flow-control.py
+++ Section_2(Python-Crash-Course)/flow-control.py
@@ -138,6 +138,15 @@ for i in range(5):
         break
     print(i)
 
+# Example 2: Python break Statement with while Loop
+# program to find first 5 multiples of 6
+i = 1
+while (i <= 10):
+    print('6 * ', (i), '=', 6 * i)
+    if i >= 5:
+        break
+    i = i + 1
+
 
 # 6. Python Pass
 # ---------------
