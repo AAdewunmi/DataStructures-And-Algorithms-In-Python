ec02785aef1438fe22caba198fbe991ec14bd074
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-21 14:36:55 +0000

    Fifty-Five Commit: Add Tutorial -> Create a Python List

---
+++ Section_2(Python-Crash-Course)/list-datatype.py
@@ -0,0 +1,33 @@
+# Python List Datatype
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python List Datatype
+# URL: https://www.programiz.com/python-programming
+
+# List Definition: A list is a collection of similar types of data. It has the following characteristics:
+# List items are ordered, changable, and allow duplicate values.
+# List items are indexed, the first item has index [0], the second item has index [1] etc
+
+# (1) Create a Python List
+# A list is created in Python by placing items inside [], separated by commas . For example,
+# A list with 3 integers
+numbers = [1, 2, 5]
+print(numbers)
+# Output: [1, 2, 5]
+
+# A list can have any number of items and they may be of different types (integer, float, string, etc.). For example,
+# empty list
+my_list = []
+# list with mixed data types
+my_list = [1, "Hello", 3.4]
+
+# (2) Access Python List Elements
+# (3) Negative Indexing in Python
+# (4) Slicing of a Python List
+# (5) Add Elements to a Python List
+# (6) Remove an Item From a List
+# (7) Iterating through a List
+# (8) Check if an Item Exists in the Python List
+# (9) Python List Length
+# (10) Python List Comprehension
+# (11) Python List Methods
