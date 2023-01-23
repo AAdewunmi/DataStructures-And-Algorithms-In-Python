e99f375a07cad1a8dddcd05b47b9e6381520c21b
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 12:48:13 +0000

    Ninty-One Commit: Add Tutorial -> Check if two sets are equal

--- Section_2(Python-Crash-Course)/set-datatype.py
+++ Section_2(Python-Crash-Course)/set-datatype.py
@@ -126,7 +126,17 @@ print('using ^:', A ^ B)
 # using symmetric_difference()
 print('using symmetric_difference():', A.symmetric_difference(B))
 
-
+# v. Check if two sets are equal
+# We can use the == operator to check whether two sets are equal or not. For example,
+# first set
+A = {1, 3, 5}
+# second set
+B = {3, 5, 1}
+# perform difference operation using &
+if A == B:
+    print('Set A and Set B are equal')
+else:
+    print('Set A and Set B are not equal')
 
 # (9) Built-in Functions with Set
 # (10) Other Python Set Methods
