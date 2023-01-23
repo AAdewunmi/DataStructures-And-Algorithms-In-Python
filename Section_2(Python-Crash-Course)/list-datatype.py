a7bfdae4008c2080326c354f98ef19082ff58e66
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 09:47:28 +0000

    Seventy Commit: Add Tutorial -> Python List Methods

--- Section_2(Python-Crash-Course)/list-datatype.py
+++ Section_2(Python-Crash-Course)/list-datatype.py
@@ -128,3 +128,27 @@ numbers = [number*number for number in range(1, 6)]
 print(numbers)
 
 # (12) Python List Methods
+# Python has many useful list methods that makes it really easy to work with lists.
+# Method        Description
+# append()
+#   add an item to the end of the list
+# extend()
+# 	add items of lists and other iterables to the end of the list
+# insert()
+# 	inserts an item at the specified index
+# remove()
+# 	removes item present at the given index
+# pop()
+# 	returns and removes item present at the given index
+# clear()
+# 	removes all items from the list
+# index()
+# 	returns the index of the first matched item
+# count()
+# 	returns the count of the specified item in the list
+# sort()
+# 	sort the list in ascending/descending order
+# reverse()
+# 	reverses the item of the list
+# copy()
+# 	returns the shallow copy of the list
