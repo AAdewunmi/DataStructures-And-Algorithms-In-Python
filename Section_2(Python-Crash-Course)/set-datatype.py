4d899887f16b0bdb0c0db5af921223a286f97cb5
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 12:50:31 +0000

    Ninty-Two Commit: Add Tutorial -> Built-in Functions with Set

--- Section_2(Python-Crash-Course)/set-datatype.py
+++ Section_2(Python-Crash-Course)/set-datatype.py
@@ -139,4 +139,32 @@ else:
     print('Set A and Set B are not equal')
 
 # (9) Built-in Functions with Set
+# Built-in functions like all(), any(), enumerate(), len(), max(), min(), sorted(), sum() etc. are commonly used with sets to perform different tasks.
+# Function
+# 					Description
+#
+# all()
+# 					Returns True if all elements of the set are true (or if the set is empty).
+#
+# any()
+# 					Returns True if any element of the set is true. If the set is empty, returns False.
+#
+# enumerate()
+# 					Returns an enumerate object. It contains the index and value for all the items of the set as a pair.
+#
+# len()
+# 					Returns the length (the number of items) in the set.
+#
+# max()
+# 					Returns the largest item in the set.
+#
+# min()
+# 					Returns the smallest item in the set.
+#
+# sorted()
+# 					Returns a new sorted list from elements in the set(does not sort the set itself).
+#
+# sum()
+# 					Returns the sum of all elements in the set.
+
 # (10) Other Python Set Methods
