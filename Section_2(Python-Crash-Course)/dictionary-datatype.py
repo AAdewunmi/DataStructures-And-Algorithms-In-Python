f4b63f7608dd182b63edd2de4799c24ebafb3c75
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 13:26:28 +0000

    Ninty-Four Commit: Add Tutorial -> Create a dictionary in Python

---
+++ Section_2(Python-Crash-Course)/dictionary-datatype.py
@@ -0,0 +1,24 @@
+# Python Dictionary Datatype
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Dictionary Datatype
+# URL: https://www.programiz.com/python-programming
+
+# Dictionary Definition: Dictionaries are used to store data values in key:value pairs. It has the following characteristics:
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+# (1) Create a dictionary in Python
+# Example i: Python Dictionary
+capital_city = {"Nepal": "Kathmandu", "Italy": "Rome", "England": "London"}
+print(capital_city)
+# dictionary with keys and values of different data types
+numbers = {1: "One", 2: "Two", 3: "Three"}
+print(numbers)
+
+# (2) Add Elements to a Python Dictionary
+# (3) Change Value of Dictionary
+# (4) Accessing Elements from Dictionary
+# (5) Removing elements from Dictionary
+# (6) Python Dictionary Methods
+# (7) Dictionary Membership Test
+# (8) Iterating Through a Dictionary
