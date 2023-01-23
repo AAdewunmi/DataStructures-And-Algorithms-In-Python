11a429d0484a93c9d8b1ec36b8ea3c3f7ea37b1a
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 12:53:27 +0000

    Ninty-Three Commit: Add Tutorial -> Other Python Set Methods

--- Section_2(Python-Crash-Course)/set-datatype.py
+++ Section_2(Python-Crash-Course)/set-datatype.py
@@ -168,3 +168,56 @@ else:
 # 					Returns the sum of all elements in the set.
 
 # (10) Other Python Set Methods
+# There are many set methods, some of which we have already used above. Here is a list of all the methods that are available with the set objects:
+# Method
+# 					Description
+#
+# add()
+# 					Adds an element to the set
+#
+# clear()
+# 					Removes all elements from the set
+#
+# copy()
+# 					Returns a copy of the set
+#
+# difference()
+# 					Returns the difference of two or more sets as a new set
+#
+# difference_update()
+# 					Removes all elements of another set from this set
+#
+# discard()
+# 					Removes an element from the set if it is a member. (Do nothing if the element is not in set)
+#
+# intersection()
+# 					Returns the intersection of two sets as a new set
+#
+# intersection_update()
+# 					Updates the set with the intersection of itself and another
+#
+# isdisjoint()
+# 					Returns True if two sets have a null intersection
+#
+# issubset()
+# 					Returns True if another set contains this set
+#
+# issuperset()
+# 					Returns True if this set contains another set
+# pop()
+# 					Removes and returns an arbitrary set element. Raises KeyError if the set is empty
+#
+# remove()
+# 					Removes an element from the set. If the element is not a member, raises a KeyError
+#
+# symmetric_difference()
+# 					Returns the symmetric difference of two sets as a new set
+#
+# symmetric_difference_update()
+# 					Updates a set with the symmetric difference of itself and another
+#
+# union()
+# 					Returns the union of sets in a new set
+#
+# update()
+# 					Updates the set with the union of itself and others
