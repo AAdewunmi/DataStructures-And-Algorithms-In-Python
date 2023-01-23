0acc3e78e43fbf8b9f8bd1b1504910868ec07453
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-23 14:36:08 +0000

    Ninty-Seven Commit: Add Tutorial -> Accessing Elements from Dictionary

--- Section_2(Python-Crash-Course)/dictionary-datatype.py
+++ Section_2(Python-Crash-Course)/dictionary-datatype.py
@@ -30,6 +30,11 @@ student_id[112] = "Stan"
 print("Updated Dictionary: ", student_id)
 
 # (4) Accessing Elements from Dictionary
+# In Python, we use the keys to access their corresponding values. For example,
+student_id = {111: "Eric", 112: "Kyle", 113: "Butters"}
+print(student_id[111])  # prints Eric
+print(student_id[113])  # prints Butters
+
 # (5) Removing elements from Dictionary
 # (6) Python Dictionary Methods
 # (7) Dictionary Membership Test
