3c5c37f83f028d928fe9ba0f2e65e1a2c351cee8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-24 15:28:02 +0000

    One Hundred-Six Commit: Create skeleton test.txt file

---
+++ Section_2(Python-Crash-Course)/(10)_python-object-and-class.py
@@ -0,0 +1,5 @@
+# Python Object and Class
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Object and Class
+# URL: https://www.programiz.com/python-programming
---
+++ Section_2(Python-Crash-Course)/(9)_python-files.py
@@ -0,0 +1,5 @@
+# Python Files
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Files
+# URL: https://www.programiz.com/python-programming
