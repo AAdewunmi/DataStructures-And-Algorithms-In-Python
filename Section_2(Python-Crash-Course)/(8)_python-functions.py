5aa20ba99f78f543dd6090bbff70f1d35859d1b8
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-24 15:41:13 +0000

    One Hundred-Eight Commit: Add Tutorial -> Opening Files in Python

--- Section_2(Python-Crash-Course)/(8)_python-functions.py
+++ Section_2(Python-Crash-Course)/(8)_python-functions.py
@@ -4,18 +4,3 @@
 # Tutorial Title: Python Functions
 # URL: https://www.programiz.com/python-programming
 
-# A file is a container in computer storage devices used for storing data.
-# When we want to read from or write to a file, we need to open it first.
-# When we are done, it needs to be closed so that the resources that are tied with the file are freed.
-# Hence, in Python, a file operation takes place in the following order:
-#     Open a file
-#     Read or write (perform operation)
-#     Close the file
-
-# (1) Opening Files in Python
-# (2) Reading Files in Python
-# (3) Closing Files in Python
-# (4) Exception Handling in Files
-# (5) Use of with...open Syntax
-# (6) Writing to Files in Python
-# (7) Python File Methods
--- Section_2(Python-Crash-Course)/(9)_python-files.py
+++ Section_2(Python-Crash-Course)/(9)_python-files.py
@@ -3,3 +3,48 @@
 # Adapted From: Programiz
 # Tutorial Title: Python Files
 # URL: https://www.programiz.com/python-programming
+
+# A file is a container in computer storage devices used for storing data.
+# When we want to read from or write to a file, we need to open it first.
+# When we are done, it needs to be closed so that the resources that are tied with the file are freed.
+# Hence, in Python, a file operation takes place in the following order:
+#     Open a file
+#     Read or write (perform operation)
+#     Close the file
+
+# (1) Opening Files in Python
+# The standard way to open data from a Python file is by using the open() function.
+file1 = open("test.txt")
+# By default, the files are open in read mode (cannot be modified). The code above is equivalent to
+file1 = open("test.txt", "r")
+# Here, we have explicitly specified the mode by passing the "r" argument which means file is opened for reading.
+#
+# Different Modes to Open a File in Python
+# Mode
+# 					Description
+# r
+# 					Open a file for reading. (default)
+# w
+# 					Open a file for writing. Creates a new file if it does not exist or truncates the file if it exists.
+# x
+# 					Open a file for exclusive creation. If the file already exists, the operation fails.
+# a
+# 					Open a file for appending at the end of the file without truncating it. Creates a new file if it does not exist.
+# t
+# 					Open in text mode. (default)
+# b
+# 					Open in binary mode.
+# +
+# 					Open a file for updating (reading and writing)
+#
+# Here's few simple examples of how to open a file in different modes,
+# file1 = open("text.txt")      # equivalent to 'r' or 'rt'
+# file1 = open("text.txt",'w')  # write in text mode
+# file1 = open("img.bmp",'r+b') # read and write in binary mode
+
+# (2) Reading Files in Python
+# (3) Closing Files in Python
+# (4) Exception Handling in Files
+# (5) Use of with...open Syntax
+# (6) Writing to Files in Python
+# (7) Python File Methods
