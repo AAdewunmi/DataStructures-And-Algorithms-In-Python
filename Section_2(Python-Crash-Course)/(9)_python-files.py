b5d5a584678ba93697629079326d36e32d55f15d
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-24 16:27:31 +0000

    One Hundred-Sixteen Commit: Add Tutorial -> Python File Methods

--- Section_2(Python-Crash-Course)/(9)_python-files.py
+++ Section_2(Python-Crash-Course)/(9)_python-files.py
@@ -104,6 +104,55 @@ finally:
 with open("test2.txt", "w") as file2:
     # write contents to the test2.txt file
     file2.write('Programming is Fun.')
-    file2.write('Programiz for beginners')
+    file2.write('Programiz for beginners and enthusiasts of programming!')
 
 # (7) Python File Methods
+# Method
+# 					Description
+#
+# close()
+# 					Closes an opened file. It has no effect if the file is already closed.
+#
+# detach()
+# 					Separates the underlying binary buffer from the TextIOBase and returns it.
+#
+# fileno()
+# 					Returns an integer number (file descriptor) of the file.
+#
+# flush()
+# 					Flushes the write buffer of the file stream.
+#
+# isatty()
+# 					Returns True if the file stream is interactive.
+#
+# read(n)
+# 					Reads at most n characters from the file. Reads till end of file if it is negative or None.
+#
+# readable()
+# 					Returns True if the file stream can be read from.
+#
+# readline(n=-1)
+# 					Reads and returns one line from the file. Reads in at most n bytes if specified.
+#
+# readlines(n=-1)
+# 					Reads and returns a list of lines from the file. Reads in at most n bytes/characters if specified.
+#
+# seek(offset,from=SEEK_SET)
+# 					Changes the file position to offset bytes, in reference to from (start, current, end).
+# seekable()
+# 					Returns True if the file stream supports random access.
+#
+# tell()
+# 					Returns an integer that represents the current position of the file's object.
+#
+# truncate(size=None)
+# 					Resizes the file stream to size bytes. If size is not specified, resizes to current location.
+#
+# writable()
+# 					Returns True if the file stream can be written to.
+#
+# write(s)
+# 					Writes the string s to the file and returns the number of characters written.
+#
+# writelines(lines)
+# 					Writes a list of lines to the file.
