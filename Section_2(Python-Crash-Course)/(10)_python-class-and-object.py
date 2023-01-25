6aa89016872e0c3c61b21d76d8264d975214c3dc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-25 09:13:47 +0000

    One Hundred-Eighteen Commit: Add Tutorial -> Python Objects

--- Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
+++ Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
@@ -12,7 +12,18 @@
 class Bike:
     name = ""
     gear = 0
+
 # (2) Python Objects
+# An object is called an instance of a class. For example, suppose Bike is a class then we can create objects like bike1, bike2, etc. from the class.
+# Here's the syntax to create an object.
+# create class
+class Bike:
+    name = ""
+    gear = 0
+# create objects of class
+bike1 = Bike()
+bike2 = Bike()
+
 # (3) Access Class Attributes Using Objects
 # (4) Create Multiple Objects of Python Class
 # (5) Python Methods
