240ae43b7ea308c23c8c1a3bf461fcdb1b58af52
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-25 09:34:25 +0000

    One Hundred-Twenty Commit: Add Tutorial -> Create Multiple Objects of Python Class

--- Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
+++ Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
@@ -38,6 +38,20 @@ bike1.name = "Mountain Bike"
 print(f"Name: {bike1.name}, Gears: {bike1.gear}")
 
 # (4) Create Multiple Objects of Python Class
+# define a class
+class Employee:
+    # define an attribute
+    employee_id = 0
+# create two objects of the Employee class
+employee1 = Employee()
+employee2 = Employee()
+# access attributes using employee1
+employee1.employee_id = 1001
+print(f"Employee ID: {employee1.employee_id}")
+# access attributes using employee1
+employee1.employee_id = 1002
+print(f"Employee ID: {employee1.employee_id}")
+
 # (5) Python Methods
 # (6) Python Constructors
 # (7) Python Inheritance
