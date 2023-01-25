52f34d8af580fd466f2dc0f1562be985f49e9c50
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-25 12:10:34 +0000

    One Hundred-Twenty-Three Commit: Add Tutorial -> Python Inheritance

--- Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
+++ Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
@@ -88,5 +88,26 @@ print_object_details(bike1)
 print_object_details(bike2)
 
 # (7) Python Inheritance
+# Inheritance is a way of creating a new class for using details of an existing class without modifying it.
+# The newly formed class is a derived class (or child class). Similarly, the existing class is a base class (or parent class).
+# Example: Use of Inheritance in Python
+# base class
+class Animal:
+    def eat(self):
+        print("I can eat!")
+    def sleep(self):
+        print("I can sleep!")
+# derived class
+class Dog(Animal):
+    def bark(self):
+        print("I can bark! Woof woof!!")
+# Create object of the Dog class
+dog1 = Dog()
+# Calling members of the base class
+dog1.eat()
+dog1.sleep()
+# Calling member of the derived class
+dog1.bark();
+
 # (8) Python Encapsulation
 # (9) Python Polymorphism
