1f800682d809cd393131df3e5d6f09bda75f03dc
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-25 08:38:37 +0000

    One Hundred-Seventeen Commit: Add Tutorial -> Python Classes

---
+++ Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
@@ -0,0 +1,22 @@
+# Python Class and Object
+# ========================
+# Adapted From: Programiz
+# Tutorial Title: Python Class and Object
+# URL: https://www.programiz.com/python-programming
+
+# Python is a versatile programming language that supports various programming styles,
+# including object-oriented programming (OOP) through the use of classes and objects.
+
+# (1) Python Classes
+# We use the class keyword to create a class in Python. For example,
+class Bike:
+    name = ""
+    gear = 0
+# (2) Python Objects
+# (3) Access Class Attributes Using Objects
+# (4) Create Multiple Objects of Python Class
+# (5) Python Methods
+# (6) Python Constructors
+# (7) Python Inheritance
+# (8) Python Encapsulation
+# (9) Python Polymorphism
--- Section_2(Python-Crash-Course)/(10)_python-object-and-class.py
+++
@@ -1,5 +0,0 @@
-# Python Object and Class
-# ========================
-# Adapted From: Programiz
-# Tutorial Title: Python Object and Class
-# URL: https://www.programiz.com/python-programming
