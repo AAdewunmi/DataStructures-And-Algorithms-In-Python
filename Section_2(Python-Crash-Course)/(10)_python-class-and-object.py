849a928ac3c51dce174fa5fd3debc5ca3945e36e
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-25 12:52:59 +0000

    One Hundred-Twenty-Five Commit: Add Tutorial -> Python Polymorphism

--- Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
+++ Section_2(Python-Crash-Course)/(10)_python-class-and-object.py
@@ -130,3 +130,25 @@ c.setMaxPrice(1050)
 c.sell()
 
 # (9) Python Polymorphism
+# Polymorphism is another important concept of object-oriented programming. It simply means more than one form.
+# That is, the same entity (method or operator or object) can perform different operations in different scenarios.
+# Let's see an example,
+class Polygon:
+    # renders Polygon
+    def render(self):
+        print("Rendering Polygon ... ")
+class Square(Polygon):
+    # renders Square
+    def render(self):
+        print("Rendering Square ... ")
+class Circle(Polygon):
+    # renders Circle
+    def render(self):
+        print("Rendering Circle ... ")
+# create an object of Square
+s1 = Square()
+s1.render()
+
+# create an object of Circle
+c1 = Circle()
+c1.render()
