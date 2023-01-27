783c09a5ace429ea2e239ac89c2c286638f90436
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-01-27 09:48:08 +0000

    One Hundred-Twenty-Seven Commit: Add Tutorial -> Python Function Declaration

--- Section_2(Python-Crash-Course)/(8)_python-functions.py
+++ Section_2(Python-Crash-Course)/(8)_python-functions.py
@@ -4,3 +4,38 @@
 # Tutorial Title: Python Functions
 # URL: https://www.programiz.com/python-programming
 
+# A function is a block of code that performs a specific task.
+# Suppose, you need to create a program to create a circle and color it. You can create two functions to solve this problem:
+#     create a circle function
+#     create a color function
+# Dividing a complex problem into smaller chunks makes our program easy to understand and reuse.
+
+# Types of function
+# There are two types of function in Python programming:
+# Standard library functions - These are built-in functions in Python that are available to use.
+# User-defined functions - We can create our own functions based on our requirements.
+
+# Python Function Declaration
+# The syntax to declare a function is:
+# def function_name(arguments):
+    # function body
+    # return
+# Here,
+#     def - keyword used to declare a function
+#     function_name - any name given to the function
+#     arguments - any value passed to function
+#     return (optional) - returns value from a function
+# Let's see an example,
+def greet():
+    print('Hello World!')
+
+# Calling a Function in Python
+# Example 1: Python Function
+# Python Function Arguments
+# Example 2: Python Function Arguments
+# The return Statement in Python
+# Example 3: Function return Type
+# Example 4: Add Two Numbers
+# Python Library Functions
+# Example 5: Python Library Function
+# Benefits of Using Functions
