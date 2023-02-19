68ec79d08c2a256ff909b7de9daf718769b18411
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-02-19 08:22:17 +0000

    Two Hundred-Nine Commit: Create skeleton ArrayDEQue Class

--- Section_6(Queue-Deque)/(2)_queue-linked-list.py
+++ Section_6(Queue-Deque)/(2)_queue-linked-list.py
@@ -1,4 +1,5 @@
 # Queue - Abstract Data Structure (ADT)
+# =====================================
 # Queue is an abstract data structure, somewhat similar to Stacks. Unlike stacks, a queue is open at both its ends. One end is always used to insert data (enqueue) and the other is used to remove data (dequeue). Queue follows First-In-First-Out methodology, i.e.,
 # the data item stored first will be accessed first. A real-world example of queue can be a single-lane one-way road, where the vehicle enters first, exits first. More real-world examples can be seen as queues at the ticket windows and bus-stops.
 #
---
+++ Section_6(Queue-Deque)/(3)_DEQue_array.py
@@ -0,0 +1,26 @@
+# DEQue (or Double Ended Queue) - Abstract Data Structure (ADT)
+# =============================================================
+#
+# Definition: In DEQue or Double Ended Queue, insertion and deletion can be done from both ends of the
+# queue either from the front or rear. It means that we can insert and delete from front and rear ends of the
+# queue. Deque can be used as a palindrome checker meaning that if we read the string from both ends,
+# then the string would be the same.
+# Deque can be used both as stack and queue as it allows the insertion and deletion operations on both ends.
+# Deque can be considered as a stack because stack follows the LIFO (Last In First Out) principle in which
+# insertion and deletion both can be performed only from one end. And in DEQue, it is possible to perform
+# both insertion and deletion from one end, and DEQue does not follow the FIFO principle.
+#
+# Representation of the DEQue:
+#
+#             Front                   Rear
+# INSERT ->  [Data][Data][Data][Data][Data] <- INSERT
+# DELETE <-                                 -> DELETE
+
+
+# Implement ArrayDEQue Class
+class ArrayDEQue:
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
