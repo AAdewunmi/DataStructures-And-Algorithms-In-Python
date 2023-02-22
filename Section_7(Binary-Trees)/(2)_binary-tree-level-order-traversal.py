16d0e611207e516e17d43286104c8d2a73d672a7
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-02-22 11:19:49 +0000

    Two Hundred - Thirty-Seven Commit: Implement levelorder() function

---
+++ Section_7(Binary-Trees)/(2)_binary-tree-level-order-traversal.py
@@ -0,0 +1,73 @@
+from queuelinkedlist import LinkedListQueue
+class _Node:
+    __slots__ = '_element', '_left', '_right'
+
+    def __init__(self, element, left=None, right=None):
+        self._element = element
+        self._left = left
+        self._right = right
+
+
+class BinaryTree:
+    def __init__(self):
+        self._root = None
+
+    def maketree(self, element, left, right):
+        self._root = _Node(element, left._root, right._root)
+
+    # Preorder: RoLR
+    # Inorder: LRoR
+    # Postorder:LRRo
+    def preorder(self, root):
+        if root:
+            print(root._element, end='-->')
+            self.preorder(root._left)
+            self.preorder(root._right)
+
+    def inorder(self, root):
+        if root:
+            self.inorder(root._left)
+            print(root._element, end='-->')
+            self.inorder(root._right)
+
+    def postorder(self, root):
+        if root:
+            self.postorder(root._left)
+            self.postorder(root._right)
+            print(root._element, end='-->')
+
+    def levelorder(self):
+       Q = LinkedListQueue()
+       t = self._root
+       print(t._element, end=' ')
+       Q.enqueue(t)
+       while not Q.is_empty():
+           t = Q.dequeue()
+           if t._left:
+               print(t._left._element, end=' ')
+               Q.enqueue(t._left)
+           if t._right:
+               print(t._right._element, end=' ')
+               Q.enqueue(t._right)
+
+x = BinaryTree()
+y = BinaryTree()
+z = BinaryTree()
+a = BinaryTree()
+b = BinaryTree()
+c = BinaryTree()
+e = BinaryTree()
+x.maketree(40, e, e)
+y.maketree(20, x, e)
+z.maketree(60, e, e)
+a.maketree(50, e, z)
+b.maketree(30, a, e)
+c.maketree(10, y, b)
+print('Preorder Traversal')
+c.preorder(c._root)
+print('\nInorder Traversal')
+c.inorder(c._root)
+print('\nPostorder Traversal')
+c.postorder(c._root)
+print('\nLevel order Traversal')
+c.levelorder()
