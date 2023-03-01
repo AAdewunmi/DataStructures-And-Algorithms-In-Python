8054302432c81d81cc0cc5bc970cc626b80ee1f6
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-03-01 15:53:29 +0000

    Two Hundred - Sixty-Nine Commit: Implement Balance the Red-Black tree after insertion

--- Section_9(Balanced-Search-Tree)/(2)_red-black-tree.py
+++ Section_9(Balanced-Search-Tree)/(2)_red-black-tree.py
@@ -187,3 +187,39 @@ class RedBlackTree():
             y.color = z.color
         if y_original_color == 0:
             self.delete_fix(x)
+
+    # Balance the tree after insertion
+    def fix_insert(self, k):
+        while k.parent.color == 1:
+            if k.parent == k.parent.parent.right:
+                u = k.parent.parent.left
+                if u.color == 1:
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.left:
+                        k = k.parent
+                        self.right_rotate(k)
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.left_rotate(k.parent.parent)
+            else:
+                u = k.parent.parent.right
+
+                if u.color == 1:
+                    u.color = 0
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    k = k.parent.parent
+                else:
+                    if k == k.parent.right:
+                        k = k.parent
+                        self.left_rotate(k)
+                    k.parent.color = 0
+                    k.parent.parent.color = 1
+                    self.right_rotate(k.parent.parent)
+            if k == self.root:
+                break
+        self.root.color = 0
