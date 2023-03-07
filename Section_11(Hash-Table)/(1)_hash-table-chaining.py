f349438678fc95f0c33de9e98bffc38547f5dbe4
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-03-07 16:09:46 +0000

    Three Hundred - Fourteen Commit: Implement display() function

--- Section_11(Hash-Table)/(1)_hash-table-chaining.py
+++ Section_11(Hash-Table)/(1)_hash-table-chaining.py
@@ -35,3 +35,20 @@ class HashChain:
     def search(self, key):
         i = self.hashcode(key)
         return self.hashtable[i].search(key) != -1
+
+    def display(self):
+        for i in range(self.hashtable_size):
+            print('[', i, ']', end=' ')
+            self.hashtable[i].display()
+        print()
+
+H = HashChain()
+H.insert(54)
+H.insert(78)
+H.insert(64)
+H.insert(92)
+H.insert(34)
+H.insert(86)
+H.insert(28)
+H.display()
+print('Search Result: ', H.search(54))
