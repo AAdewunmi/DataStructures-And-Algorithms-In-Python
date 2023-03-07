346b7e902e9d3d72a327ba34cd8a9c99004417ab
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-03-07 13:41:11 +0000

    Three Hundred - Nine Commit: Implement HashChain Class

--- Section_11(Hash-Table)/(1)_hash-table-chaining.py
+++ Section_11(Hash-Table)/(1)_hash-table-chaining.py
@@ -17,7 +17,10 @@
 # Implement Hashing using Chaining method
 # Chains are represented using LinkedList
 
+from linkedlist import LinkedList
 class HashChain:
     def __init__(self):
         self.hashtable_size = 10
         self.hashtable = [0] * self.hashtable_size
+        for i in range(self.hashtable_size):
+            self.hashtable[i] = LinkedList()
