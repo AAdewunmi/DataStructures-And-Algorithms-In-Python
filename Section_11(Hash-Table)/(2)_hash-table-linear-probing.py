a6cad9831588883576f890cd0eb2e878eb528ebf
adrian adewunmi <adrian.a.adewunmi@googlemail.com>
2023-03-08 10:06:05 +0000

    Three Hundred - Seventeen Commit: Implement hashcode() function

--- Section_11(Hash-Table)/(2)_hash-table-linear-probing.py
+++ Section_11(Hash-Table)/(2)_hash-table-linear-probing.py
@@ -14,3 +14,6 @@ class HashTableLinearProbe:
     def __init__(self):
         self.hashtable_size = 10
         self.hashtable = [0] * self.hashtable_size
+
+    def hashcode(self, key):
+        return key % self.hashtable_size
